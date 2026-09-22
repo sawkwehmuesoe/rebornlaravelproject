08ccf5105f1bcc24eef34477bb4a26fb06253780
Saw Kweh Mue Soe <sawkwehmuesoe@gmail.com>
2026-09-22 20:08:55 +0700

    Day 6

---
+++ lesson/lessonone/resources/views/aboutcompany.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>About Company</title>
+    </head>
+    <body>
+
+        <h1>About Company</h1>
+
+    </body>
+</html>
--- lesson/lessonone/routes/web.php
+++ lesson/lessonone/routes/web.php
@@ -2,6 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return "Save Myanmar";
 });
+
+Route::get('/sayar',function(){
+    return "Hay, Sayar Nay Kaung Lar";
+});
+
+Route::get('/sayhi',function(){
+    return "Hi, Min Gar Lar Par";
+});
+
+// Route::get('/about',function(){
+//     return view("aboutme");
+// });
+
+Route::view('about',"aboutme");
+
+Route::view('/about/company',"aboutcompany");
+
+// => Redirect 
+
+// Route::get('/contact',function(){
+//     return redirect("/about");
+// });
+
+Route::redirect('/contact','/about/company');
+
+
+
